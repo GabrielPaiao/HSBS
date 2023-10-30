2bafa540758db0019bf303231521356d1551a76f
Gabriel Paião <bielpaiao8@gmail.com>
2023-10-30 20:54:30 -0300

    código inicial do webscrapping
    andré desenvolveu o código base e gabriel alterou para verificar se as classes css buscadas ainda existiam durante a execução

---
+++ main.py
@@ -0,0 +1,29 @@
+#codigo andré, alterado pelo gabriel para verificar se as classes ainda existem durante a execução
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.kabum.com.br/computadores/pc'
+response = requests.get(url)
+
+titulos_pc = []
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    computadores_div = soup.find_all('div', class_='sc-93fa31de-15 dCsZrx')
+
+    if not computadores_div:
+        print('Nenhuma div encontrada com a classe especificada.')
+    else:
+        for div in computadores_div:
+            titulos = div.find_all('h2')
+            for titulo in titulos:
+                titulos_pc.append(titulo.text)
+else:
+    print('Falha ao carregar a página. Código de status:', response.status_code)
+
+if not titulos_pc:
+    print('Nenhum título de computador encontrado.')
+else:
+    for nome_configs in titulos_pc:
+        print(nome_configs)
